452a46f72027a8a29c2223544cfbe88e5cdd11df
gromozeka07b9 <dserega@rambler.ru>
2015-12-18 00:07:03 +0300

    эксперименты с размером экрана

--- TranslateHelper.Droid/DictionaryActivity.cs
+++ TranslateHelper.Droid/DictionaryActivity.cs
@@ -21,19 +21,19 @@ using TranslateHelper.Core.Helpers;
 
 namespace TranslateHelper.Droid
 {
-    [Activity (Label = "Translate helper", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
-    public class DictionaryActivity : Activity
+	[Activity (Label = "Translate helper", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+	public class DictionaryActivity : Activity
 	{
-        List<TranslateResult> items;
+		List<TranslateResult> items;
 
-        protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate (Bundle bundle)
 		{
-            base.OnCreate (bundle);
+			base.OnCreate (bundle);
 			base.ActionBar.NavigationMode = ActionBarNavigationMode.Standard;
 			SetContentView (Resource.Layout.Dictionary);
 			
 
-            EditText editSourceText = FindViewById<EditText> (Resource.Id.textSourceString);
+			EditText editSourceText = FindViewById<EditText> (Resource.Id.textSourceString);
 			ImageButton buttonNew = FindViewById<ImageButton> (Resource.Id.buttonNew);
 			ImageButton buttonTranslate = FindViewById<ImageButton> (Resource.Id.buttonTranslate);
 			ListView resultListView = FindViewById<ListView> (Resource.Id.listResultListView);
@@ -43,197 +43,194 @@ namespace TranslateHelper.Droid
 			buttonNew.Click += (object sender, EventArgs e) => {
 				{
 					editSourceText.Text = string.Empty;
-					clearTraslatedRegion();
+					clearTraslatedRegion ();
 				}
 			};
 
-            //ToDo:Поправить жесткий копипаст
-            buttonTranslate.Click += async (object sender, EventArgs e) =>
-            {
-                //string sourceText = editSourceText.Text.Replace('\n', ' ').Trim().ToLower();
-                string sourceText = ConvertStrings.StringToOneLowerLineWithTrim(editSourceText.Text);
-                if (sourceText.Length > 0)
-                {
-                    IRequestTranslateString translaterFromCache = new LocalDatabaseCache();
-                    var resultFromCache = await translaterFromCache.Translate(sourceText, "en-ru");
-                    if (resultFromCache.translateResult.Collection.Count > 0)
-                    {
-                        UpdateListResults(sourceText, resultFromCache, false);
-                    }
-                    else
-                    {
-                        IRequestTranslateString translaterDict = new TranslateRequest(TypeTranslateServices.YandexDictionary);
-                        var resultDict = await translaterDict.Translate(sourceText, "en-ru");
-                        if (resultDict.translateResult.Collection.Count > 0)
-                        {
-                            UpdateListResults(sourceText, resultDict, true);
-                        }
-                        else
-                        {
-                            IRequestTranslateString translaterTranslate = new TranslateRequest(TypeTranslateServices.YandexTranslate);
-                            var resultTrans = await translaterTranslate.Translate(sourceText, "en-ru");
-                            UpdateListResults(sourceText, resultTrans, true);
-                        }
-                    }
-                }
-            };
+			//ToDo:Поправить жесткий копипаст
+			buttonTranslate.Click += async (object sender, EventArgs e) => {
+				//string sourceText = editSourceText.Text.Replace('\n', ' ').Trim().ToLower();
+				string sourceText = ConvertStrings.StringToOneLowerLineWithTrim (editSourceText.Text);
+				if (sourceText.Length > 0) {
+					IRequestTranslateString translaterFromCache = new LocalDatabaseCache ();
+					var resultFromCache = await translaterFromCache.Translate (sourceText, "en-ru");
+					if (resultFromCache.translateResult.Collection.Count > 0) {
+						UpdateListResults (sourceText, resultFromCache, false);
+					} else {
+						IRequestTranslateString translaterDict = new TranslateRequest (TypeTranslateServices.YandexDictionary);
+						var resultDict = await translaterDict.Translate (sourceText, "en-ru");
+						if (resultDict.translateResult.Collection.Count > 0) {
+							UpdateListResults (sourceText, resultDict, true);
+						} else {
+							IRequestTranslateString translaterTranslate = new TranslateRequest (TypeTranslateServices.YandexTranslate);
+							var resultTrans = await translaterTranslate.Translate (sourceText, "en-ru");
+							UpdateListResults (sourceText, resultTrans, true);
+						}
+					}
+				}
+			};
 
-            //ToDo:Поправить жесткий копипаст
+			//ToDo:Поправить жесткий копипаст
 			editSourceText.TextChanged += async (object sender, Android.Text.TextChangedEventArgs e) => {
                 
-                if ((editSourceText.Text.Length > 0) && (iSSymbolForStartTranslate (editSourceText.Text.Last ()))) {
-                    //ToDo:убрать перевод строки в контроле
-                    //string sourceText = editSourceText.Text.Replace('\n', ' ').Trim().ToLower();
-                    string sourceText = ConvertStrings.StringToOneLowerLineWithTrim(editSourceText.Text);
-                    if (sourceText.Length > 0)
-                    {
-                        IRequestTranslateString translaterFromCache = new LocalDatabaseCache();
-                        var resultFromCache = await translaterFromCache.Translate(sourceText, "en-ru");
-                        if (resultFromCache.translateResult.Collection.Count > 0)
-                        {
-                            UpdateListResults(sourceText, resultFromCache, false);
-                        }
-                        else
-                        {
-                            IRequestTranslateString translaterDict = new TranslateRequest(TypeTranslateServices.YandexDictionary);
-                            var resultDict = await translaterDict.Translate(sourceText, "en-ru");
-                            if (resultDict.translateResult.Collection.Count > 0)
-                            {
-                                UpdateListResults(sourceText, resultDict, true);
-                            }
-                            else
-                            {
-                                IRequestTranslateString translaterTranslate = new TranslateRequest(TypeTranslateServices.YandexTranslate);
-                                var resultTrans = await translaterTranslate.Translate(sourceText, "en-ru");
-                                UpdateListResults(sourceText, resultTrans, true);
-                            }
-                        }
-                    }
-                }
-            };
+				if ((editSourceText.Text.Length > 0) && (iSSymbolForStartTranslate (editSourceText.Text.Last ()))) {
+					//ToDo:убрать перевод строки в контроле
+					//string sourceText = editSourceText.Text.Replace('\n', ' ').Trim().ToLower();
+					string sourceText = ConvertStrings.StringToOneLowerLineWithTrim (editSourceText.Text);
+					if (sourceText.Length > 0) {
+						IRequestTranslateString translaterFromCache = new LocalDatabaseCache ();
+						var resultFromCache = await translaterFromCache.Translate (sourceText, "en-ru");
+						if (resultFromCache.translateResult.Collection.Count > 0) {
+							UpdateListResults (sourceText, resultFromCache, false);
+						} else {
+							IRequestTranslateString translaterDict = new TranslateRequest (TypeTranslateServices.YandexDictionary);
+							var resultDict = await translaterDict.Translate (sourceText, "en-ru");
+							if (resultDict.translateResult.Collection.Count > 0) {
+								UpdateListResults (sourceText, resultDict, true);
+							} else {
+								IRequestTranslateString translaterTranslate = new TranslateRequest (TypeTranslateServices.YandexTranslate);
+								var resultTrans = await translaterTranslate.Translate (sourceText, "en-ru");
+								UpdateListResults (sourceText, resultTrans, true);
+							}
+						}
+					}
+				}
+			};
 
 			resultListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
-                var item = resultListView.GetItemAtPosition (e.Position).Cast<TranslateResult>();
-                AddToFavorites(ConvertStrings.StringToOneLowerLineWithTrim(editSourceText.Text), item);
+				var item = resultListView.GetItemAtPosition (e.Position).Cast<TranslateResult> ();
+				var favStatePic = e.View.FindViewById<ImageView> (Resource.Id.FavoritesStatePic);
+				favStatePic.SetImageResource (Resource.Drawable.v3alreadyaddedtofav);
+				AddToFavorites (ConvertStrings.StringToOneLowerLineWithTrim (editSourceText.Text), item);
 			};
 
 			clearTraslatedRegion ();
 		}
 
-        private void clearTraslatedRegion()
+		private void clearTraslatedRegion ()
 		{
-            var ListResultStrings = new List<string>();
-            ListView lv = FindViewById<ListView>(Resource.Id.listResultListView);
-            lv.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, ListResultStrings.ToArray());
-            TextView splash = FindViewById<TextView>(Resource.Id.splashTextView);
-            splash.Visibility = ViewStates.Visible;
-        }
+			var ListResultStrings = new List<string> ();
+			ListView lv = FindViewById<ListView> (Resource.Id.listResultListView);
+			lv.Adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, ListResultStrings.ToArray ());
+			TextView splash = FindViewById<TextView> (Resource.Id.splashTextView);
+			splash.Visibility = ViewStates.Visible;
+			ShowScreenResolution ();
+		}
 
 		private bool iSSymbolForStartTranslate (char p)
 		{
 			return ((p == ' ') || (p == '\n'));
-        }
+		}
 
-        //ToDo: разнести по разным методам вывод и запись в кэш
-        void UpdateListResults(string sourceText, TranslateRequestResult result, bool addToLocalCache)
-        {
-            if (string.IsNullOrEmpty(result.errorDescription))
-            {
-                if(result.translateResult.Collection.Count > 0)
-                {
-                    TextView splash = FindViewById<TextView>(Resource.Id.splashTextView);
-                    splash.Visibility = ViewStates.Invisible;
-                    var listView = FindViewById<ListView>(Resource.Id.listResultListView);
-                    items = result.translateResult.Collection;
-                    listView.Adapter = new TranslateResultAdapter(this, items);
-                    if (addToLocalCache)
-                    {
-                        AddResultToLocalCache(sourceText, result.translateResult.Collection);
-                    }
-                }
-                else
-                {
-                    Android.Widget.Toast.MakeText(this, "Неизвестное выражение, проверьте текст на наличие ошибок.", Android.Widget.ToastLength.Long).Show();
-                }
-            }
-            else
-            {
-                Android.Widget.Toast.MakeText(this, "Ошибка подключения к интернет", Android.Widget.ToastLength.Short).Show();
-            }
-        }
+		//ToDo: разнести по разным методам вывод и запись в кэш
+		void UpdateListResults (string sourceText, TranslateRequestResult result, bool addToLocalCache)
+		{
+			if (string.IsNullOrEmpty (result.errorDescription)) {
+				if (result.translateResult.Collection.Count > 0) {
+					TextView splash = FindViewById<TextView> (Resource.Id.splashTextView);
+					splash.Visibility = ViewStates.Invisible;
+					var listView = FindViewById<ListView> (Resource.Id.listResultListView);
+					items = result.translateResult.Collection;
+					listView.Adapter = new TranslateResultAdapter (this, items);
+					if (addToLocalCache) {
+						AddResultToLocalCache (sourceText, result.translateResult.Collection);
+					}
+				} else {
+					Android.Widget.Toast.MakeText (this, "Неизвестное выражение, проверьте текст на наличие ошибок.", Android.Widget.ToastLength.Long).Show ();
+				}
+			} else {
+				Android.Widget.Toast.MakeText (this, "Ошибка подключения к интернет", Android.Widget.ToastLength.Short).Show ();
+			}
+		}
 
-        private void AddResultToLocalCache(string sourceText, List<TranslateResult> resultList)
-        {
-            if(resultList.Count > 0)
-            {
-                Core.TranslatedExpressionManager manager = new Core.TranslatedExpressionManager();
-                //ToDo:передавать дерево результатов
-                manager.AddNewWord(sourceText, resultList);
-            }
-        }
+		private void AddResultToLocalCache (string sourceText, List<TranslateResult> resultList)
+		{
+			if (resultList.Count > 0) {
+				Core.TranslatedExpressionManager manager = new Core.TranslatedExpressionManager ();
+				//ToDo:передавать дерево результатов
+				manager.AddNewWord (sourceText, resultList);
+			}
+		}
 
-        private async void AddToFavorites(string sourceText, TranslateResult result)
-        {
-            Core.SourceExpressionManager sourceExprManager = new SourceExpressionManager();
-            IEnumerable<SourceExpression> sourceEnumerator = sourceExprManager.GetItemsForText(sourceText);
-            List<SourceExpression> listSourceExpr = sourceEnumerator.ToList<SourceExpression>();
-            if (listSourceExpr.Count > 0)
-            {
-                int sourceId = listSourceExpr[0].ID;
-                Core.TranslatedExpressionManager transExprManager = new Core.TranslatedExpressionManager();
-                IEnumerable<TranslatedExpression> transEnumerator = transExprManager.GetTranslateResultFromLocalCache(sourceId);
-                var transExprItem = transEnumerator.Where(item => item.TranslatedText == result.TranslatedText).Single<TranslatedExpression>();
-                Core.FavoritesManager favoritesManager = new FavoritesManager();
-                favoritesManager.AddNewWord(transExprItem.ID);
-                Android.Widget.Toast.MakeText(this, "Элемент добавлен в избранное", Android.Widget.ToastLength.Short).Show();
+		private async void AddToFavorites (string sourceText, TranslateResult result)
+		{
+			if (result.FavoritesId == 0) {
+				Core.SourceExpressionManager sourceExprManager = new SourceExpressionManager ();
+				IEnumerable<SourceExpression> sourceEnumerator = sourceExprManager.GetItemsForText (sourceText);
+				List<SourceExpression> listSourceExpr = sourceEnumerator.ToList<SourceExpression> ();
+				if (listSourceExpr.Count > 0) {
+					int sourceId = listSourceExpr [0].ID;
+					Core.TranslatedExpressionManager transExprManager = new Core.TranslatedExpressionManager ();
+					IEnumerable<TranslatedExpression> transEnumerator = transExprManager.GetTranslateResultFromLocalCache (sourceId);
+					var transExprItem = transEnumerator.Where (item => item.TranslatedText == result.TranslatedText).Single<TranslatedExpression> ();
+					Core.FavoritesManager favoritesManager = new FavoritesManager ();
+					favoritesManager.AddNewWord (transExprItem.ID);
+					Android.Widget.Toast.MakeText (this, "Элемент добавлен в избранное", Android.Widget.ToastLength.Short).Show ();
 
-                IRequestTranslateString translaterFromCache = new LocalDatabaseCache();
-                EditText editSourceText = FindViewById<EditText>(Resource.Id.textSourceString);
-                var resultFromCache = await translaterFromCache.Translate(sourceText, "en-ru");
-                if (resultFromCache.translateResult.Collection.Count > 0)
-                {
-                    UpdateListResults(sourceText, resultFromCache, false);
-                }
+					/*IRequestTranslateString translaterFromCache = new LocalDatabaseCache ();
+					EditText editSourceText = FindViewById<EditText> (Resource.Id.textSourceString);
+					var resultFromCache = await translaterFromCache.Translate (sourceText, "en-ru");
+					if (resultFromCache.translateResult.Collection.Count > 0) {
+						UpdateListResults (sourceText, resultFromCache, false);
+					}*/
+				}
+			} else {
+				Android.Widget.Toast.MakeText (this, "Элемент уже в избранном!", Android.Widget.ToastLength.Short).Show ();
+			}
 
-            }
-        }
+		}
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_DictionaryScreen, menu);
-            return true;
-        }
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+			MenuInflater.Inflate (Resource.Menu.menu_DictionaryScreen, menu);
+			return true;
+		}
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            switch (item.ItemId)
-            {
-                case Resource.Id.menu_favorites:
-                    StartActivity(typeof(FavoritesActivity));
-                    return true;
-                case Resource.Id.menu_testing:
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			switch (item.ItemId) {
+			case Resource.Id.menu_favorites:
+				StartActivity (typeof(FavoritesActivity));
+				return true;
+			case Resource.Id.menu_testing:
                     //StartActivity(typeof(FavoritesActivity));
-                    return true;
-                case Resource.Id.menu_settings:
-                    StartActivity(typeof(SettingsActivity));
-                    return true;
-                case global::Android.Resource.Id.Home:
-                    StartActivity(typeof(SettingsActivity));
-                    return true;
-                default:
-                    break;
-            }
-            return true;
-        }
+				return true;
+			case Resource.Id.menu_settings:
+				StartActivity (typeof(SettingsActivity));
+				return true;
+			case global::Android.Resource.Id.Home:
+				StartActivity (typeof(SettingsActivity));
+				return true;
+			default:
+				break;
+			}
+			return true;
+		}
 
-        void MenuItemClicked(string item)
-        {
-            Console.WriteLine(item + " option menuitem clicked");
-            var t = Toast.MakeText(this, "Options Menu '" + item + "' clicked", ToastLength.Short);
-            t.SetGravity(GravityFlags.Center, 0, 0);
-            t.Show();
-        }
+		void MenuItemClicked (string item)
+		{
+			Console.WriteLine (item + " option menuitem clicked");
+			var t = Toast.MakeText (this, "Options Menu '" + item + "' clicked", ToastLength.Short);
+			t.SetGravity (GravityFlags.Center, 0, 0);
+			t.Show ();
+		}
 
-    }
+		private int ConvertPixelsToDp (float pixelValue)
+		{
+			var dp = (int)((pixelValue) / Resources.DisplayMetrics.Density);
+			return dp;
+		}
+
+		void ShowScreenResolution ()
+		{
+			var metrics = Resources.DisplayMetrics;
+			var widthInDp = ConvertPixelsToDp (metrics.WidthPixels);
+			var heightInDp = ConvertPixelsToDp (metrics.HeightPixels);
+			TextView splashTextView = FindViewById<TextView> (Resource.Id.splashTextView);
+			string px = "px:" + metrics.WidthPixels.ToString () + "x" + metrics.HeightPixels.ToString ();
+			string dp = "dp:" + widthInDp.ToString () + "x" + heightInDp.ToString ();
+			splashTextView.Text = "Готов что-нибудь перевести!" + System.Environment.NewLine + px + System.Environment.NewLine + dp;
+		}
+	}
 }
 
